7b28ecad6e3b2dabe89158da6890278fb713b05a
Hamidou-ctr <hamidou-diallo@gmx.de>
2024-10-13 10:31:10 +0200

    Refactor level class and add new subclasses
    - Refactored the Level class to improve code organization and readability.
    - Added new subclasses: Cloud, BackgroundObject, StatusBar, StatusBarEndboss, StatusBarCoins, StatusBarBottle, SalsaBottles, and Coins.
    - Each subclass represents a different game element and inherits properties and methods from the appropriate parent class.

---
+++ models/chicken.class.js
@@ -0,0 +1,93 @@
+class Chicken extends MovableObject {
+    height = 70;
+    width = 80;
+    y = 355;
+    chickenDead = false;
+    removalDelay = 1000;
+
+    IMAGES_WALKING = [
+        "assets/img_pollo_locco/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+        "assets/img_pollo_locco/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+        "assets/img_pollo_locco/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+    ];
+
+    DEAD_CHICKEN = ["assets/img_pollo_locco/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
+
+    deadChickenSound;
+
+    offset = {
+        top: 5,
+        left: 10,
+        right: 10,
+        bottom: 5,
+    };
+
+    constructor() {
+        super();
+        this.loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.DEAD_CHICKEN);
+
+        this.deadChickenSound = new Audio("audio/deadChicken.mp3");
+        sounds.push(this.deadChickenSound);
+
+        this.x = 400 + Math.random() * 1800;
+        this.speed = 0.15 + Math.random() * 0.5;
+
+        this.animate();
+    }
+
+    /**
+     * Handles the hit action when the chicken is hit by an object or attack.
+     * Marks the chicken as dead, stops its movement, triggers the dead animation, plays the dead sound,
+     * and removes the chicken from the world after a specified delay.
+     *
+     * @method hit
+     */
+    hit() {
+        this.chickenDead = true;
+        this.speed = 0;
+        this.deadChicken();
+
+        // Play sound indicating the chicken is dead
+        this.deadChickenSound.play();
+
+        // Remove the chicken from the world after a delay
+        setTimeout(() => {
+            this.chickenDead = true;
+            this.removeFromWorld = true;
+        }, this.removalDelay);
+    }
+
+    /**
+     * Plays the dead animation for the chicken.
+     * This is triggered when the chicken dies.
+     *
+     * @method deadChicken
+     */
+    deadChicken() {
+        this.playAnimation(this.DEAD_CHICKEN);
+    }
+
+    /**
+     * Animates the chicken by handling movement and animation frame updates.
+     * The chicken moves to the left if it's not dead and plays the walking animation periodically.
+     *
+     * @method animate
+     */
+    animate() {
+        // Handle movement if the chicken is not dead
+        setInterval(() => {
+            if (!this.chickenDead) {
+                this.moveLeft();
+            }
+        }, 1000 / 60);
+
+        // Handle animation frames for walking if the chicken is not dead
+        setInterval(() => {
+            if (!this.chickenDead) {
+                this.playAnimation(this.IMAGES_WALKING);
+            }
+        }, 130);
+    }
+}
